60ac2434dc8993dfa36cc5f6ed4f07c35d62ff13
Suzii <z.dankovcikova@gmail.com>
2020-08-22 20:21:44 +0200

    Set up Kontent integration

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This repository contains source code of www.drafter.sk website built in GatsbyJS, using Kentico Kontent Headless CMS as a source.
+
+## Setup
+Provide `.env.development` and/or `.env.production` files in the root of the project with following properties.
+
+```
+KONTENT_PREVIEW_API_KEY=<yout-preview-api-key>
+```
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,28 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const config = require('gatsby-plugin-config').default;
+const usePreviewApi = config.NODE_ENV === 'development';
+
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-plugin-typescript`,
+      resolve: 'gatsby-plugin-typescript',
       options: {
         isTSX: true,
-        jsxPragma: `React`,
+        jsxPragma: 'React',
         allExtensions: true,
+      },
+    },
+    {
+      resolve: '@kentico/gatsby-source-kontent',
+      options: {
+        projectId: 'f408ff21-bb77-0017-334f-f1ea95739f2a',
+        languageCodenames: [`sk-SK`],
+        includeTaxonomies: true,
+        usePreviewUrl: usePreviewApi,
+        authorizationKey: usePreviewApi ? config.KONTENT_PREVIEW_API_KEY : undefined,
       },
     },
   ],
