cb1f00afd43763fcd281bd1fdc1a75943965cf39
Suzii <z.dankovcikova@gmail.com>
2020-08-22 19:46:14 +0200

    FIxup first commit

---
+++ src/components/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Layout: React.FC = ({ children }) => (
+  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+    {children}
+  </div>
+)
+
+export default Layout;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Home: React.FC = () => (
+  <Layout>
+    <div>Hello, from drafter.sk!</div>
+  </Layout>
+)
+
+export default Home
