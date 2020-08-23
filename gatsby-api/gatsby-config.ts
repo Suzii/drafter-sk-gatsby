925794527dbecb7eb5642c489dabbfe935bea76a
Suzii <z.dankovcikova@gmail.com>
2020-08-23 14:52:49 +0200

    Install ts-node to be able to write gatsby-* files in typescript

---
+++ gatsby-api/gatsby-config.ts
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const config = require('gatsby-plugin-config').default
+const usePreviewApi = config.NODE_ENV === 'development'
+
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        jsxPragma: 'React',
+        allExtensions: true,
+      },
+    },
+    {
+      resolve: '@kentico/gatsby-source-kontent',
+      options: {
+        projectId: 'f408ff21-bb77-0017-334f-f1ea95739f2a',
+        languageCodenames: [`sk-SK`],
+        includeTaxonomies: true,
+        usePreviewUrl: usePreviewApi,
+        authorizationKey: usePreviewApi ? config.KONTENT_PREVIEW_API_KEY : undefined,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-graphql-codegen`,
+      options: {
+        fileName: `./graphql-types.ts`,
+        documentPaths: [
+          './src/**/*.{ts,tsx}',
+          './gatsby-api/**/*.{ts,tsx}'
+        ],
+        codegenPlugins: [
+          {
+            resolve: 'typescript',
+          },
+        ],
+      },
+    },
+  ],
+}
