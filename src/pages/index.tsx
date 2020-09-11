4b264f34dda39eea8aad6f9d6b800e8d3bf031cb
Suzii <z.dankovcikova@gmail.com>
2020-09-11 08:59:55 +0200

    Migrate content of Who-we-are sectio (no styling)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import { HeroSection } from '../components/sections/HeroSection';
+import { WhoWeAreSection } from '../components/sections/WhoWeAreSection';
 
 const Home: React.FC = () => (
   <Layout>
     <HeroSection />
+    <WhoWeAreSection />
   </Layout>
 );
 
