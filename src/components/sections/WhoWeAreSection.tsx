1dc8d1ee18e95fbc2e705283ca4e21a2b4caf4b6
Suzii <z.dankovcikova@gmail.com>
2020-09-11 12:07:24 +0200

    Fixup Who We Are section

--- src/components/sections/WhoWeAreSection.tsx
+++ src/components/sections/WhoWeAreSection.tsx
@@ -1,47 +1,58 @@
 import React from 'react';
-import { Container, Row, Section, SectionTitle } from '../../_ui-components/Container';
+import { Container, Row, Section, SectionTitle, Stack } from '../../_ui-components/Container';
 import { WHO_WE_ARE_URL } from '../../constants/urls';
 import { stripSlashes } from '../../utils/urlUtils';
 
 export const WhoWeAreSection: React.FC = () => (
   <Section isFullHeight id={stripSlashes(WHO_WE_ARE_URL)}>
     <Container>
-      <Row>
-        <SectionTitle>Kto sme</SectionTitle>
-      </Row>
-      <Row>
-        <p>
-          Spoločnosť DRAFTER,s.r.o. Vám ponúka široký sortiment potravinárskych výrobkov, zameraný hlavne na syrové špeciaity.
-        </p>
-        <p>
-          Z našej ponuky si môžete vybrať výrobky z kravského, ovčieho ako aj z kozieho mlieka od našich výrobcov, šikovných farmárov ale aj od top zahraničných producentov. Okrem mliečnych výrobkov u nás nájdete aj mäsové výrobky spoločnosti Mäsovýroba Hôrka, ktorá vsadila na ich tradičné a poctivé spracovanie. Od roku 2017 dovážame do Slovenskej republiky špičkové produkty od spoločnosti TRGOPPRODUCT z Macedónska pod ich exkluzívnou značkou <a href="/files/mamas_v2.pdf" target='_blank'>MAMA´S</a>. Ide o pochutiny ako je Ajvar, Zelený ajvar, Lutenica a ďalšie.
-        </p>
-        <p>
-          Náš tovar dodávame do obchodných sietí v Slovenskej republike, Českej republike a v Maďarsku, ako aj do nezávislých sietí, obchodov a špecializovaných predajní. Portfólio tovaru neustále dopĺňame a vyberáme pre Vás len to najlepšie zo Slovenska a zahraničia. O naše dlhoročné skúsenosti v mliekárenskej výrobe a obchode sa radi podelíme so začínajúcimi výrobcami formou poradenstva.
-        </p>
-      </Row>
-      <Row>
-        {features.map((feature, index) => (
-          <Feature feature={feature} key={index} />
-        ))}
-      </Row>
-      <Row>
-        <p>
-          <b>Radi Vás privítame do rodiny našich spokojných zákazníkov.</b>
-        </p>
-      </Row>
+      <Stack>
+        <Row>
+          <SectionTitle>Kto sme</SectionTitle>
+        </Row>
+        <Stack>
+          <p>
+            Spoločnosť DRAFTER,s.r.o. Vám ponúka široký sortiment potravinárskych výrobkov, zameraný hlavne na syrové
+            špeciaity.
+          </p>
+          <p>
+            Z našej ponuky si môžete vybrať výrobky z kravského, ovčieho ako aj z kozieho mlieka od našich výrobcov,
+            šikovných farmárov ale aj od top zahraničných producentov. Okrem mliečnych výrobkov u nás nájdete aj mäsové
+            výrobky spoločnosti Mäsovýroba Hôrka, ktorá vsadila na ich tradičné a poctivé spracovanie. Od roku 2017
+            dovážame do Slovenskej republiky špičkové produkty od spoločnosti TRGOPPRODUCT z Macedónska pod ich
+            exkluzívnou značkou <a href="/files/mamas_v2.pdf" target='_blank'>MAMA´S</a>. Ide o pochutiny ako je Ajvar,
+            Zelený ajvar, Lutenica a ďalšie.
+          </p>
+          <p>
+            Náš tovar dodávame do obchodných sietí v Slovenskej republike, Českej republike a v Maďarsku, ako aj do
+            nezávislých sietí, obchodov a špecializovaných predajní. Portfólio tovaru neustále dopĺňame a vyberáme pre
+            Vás len to najlepšie zo Slovenska a zahraničia. O naše dlhoročné skúsenosti v mliekárenskej výrobe a obchode
+            sa radi podelíme so začínajúcimi výrobcami formou poradenstva.
+          </p>
+        </Stack>
+        <Row>
+          {features.map((feature, index) => (
+            <Feature feature={feature} key={index} />
+          ))}
+        </Row>
+        <Row>
+          <p>
+            <b>Radi Vás privítame do rodiny našich spokojných zákazníkov.</b>
+          </p>
+        </Row>
+      </Stack>
     </Container>
   </Section>
 );
 
 type FeatureType = Readonly<{ title: string; icon: string; }>;
 const features: readonly FeatureType[] = [
-  {title: 'Dlhoročné skúsenosti', icon: '.fas.fa-4x.fa-thumbs-up'},
-  {title: 'Férový prístup', icon: '.fas.fa-4x.fa-handshake'},
-  {title: 'Spokojní zákazníci', icon: '.fas.fa-4x.fa-users'},
+  { title: 'Dlhoročné skúsenosti', icon: '.fas.fa-4x.fa-thumbs-up' },
+  { title: 'Férový prístup', icon: '.fas.fa-4x.fa-handshake' },
+  { title: 'Spokojní zákazníci', icon: '.fas.fa-4x.fa-users' },
 ];
 
-const Feature: React.FC<{readonly feature: FeatureType}> = ({ feature: { title, icon } }) => (
+const Feature: React.FC<{ readonly feature: FeatureType }> = ({ feature: { title, icon } }) => (
   <div>
     <i className={icon} />
     <h4>{title}</h4>
