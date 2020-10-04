787369d0ff0b110ffa4492e1ccd9505e0333acab
Suzii <z.dankovcikova@gmail.com>
2020-10-04 16:46:07 +0200

    Add proper <head> attributes

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,7 +5,20 @@ const HtmlAttributes = {
 };
 
 const HeadComponents = [
-  <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+  <meta charSet="UTF-8" />,
+  <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />,
+  <meta httpEquiv="X-UA-Compatible" content="ie=edge" />,
+  <title>Drafter, s.r.o.</title>,
+  <meta name="author" content="suzii" />,
+  <meta name="description" content="Drafter - farmárske produkty, od výrobcov až na vaše pulty." />,
+  <meta name="keywords" content="mliečne výrobky, syry, kozie, ovčie, ajvar, mlieko, bryndza, jogurty, oštiepok" />,
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />,
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />,
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />,
+  <link rel="manifest" href="/site.webmanifest" />,
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffc914" />,
+  <meta name="msapplication-TileColor" content="#26471f" />,
+  <meta name="theme-color" content="#ffffff" />,
 ];
 
 const PreviewBodyAttributes = {
