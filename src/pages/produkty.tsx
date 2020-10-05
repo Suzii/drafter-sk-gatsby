ba16a2e057b8c9a0ef3ba1327c2aa3642e8c0517
Suzii <z.dankovcikova@gmail.com>
2020-10-05 18:36:38 +0200

    Refactor products listing using material-ui grid and fluid images

--- src/pages/produkty.tsx
+++ src/pages/produkty.tsx
@@ -1,7 +1,12 @@
+import { Typography } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import { graphql, Link } from 'gatsby';
+import Img, { FluidObject } from 'gatsby-image';
 import React from 'react';
 import { ProductsQuery } from '../../graphql-types';
-import { Container, Row, Stack } from '../_ui-components/Container';
+import { Section, SectionTitle } from '../_ui-components/Container';
 import Layout from '../components/Layout';
 import { PRODUCT_URL } from '../constants/urls';
 import { Feature, isFeatureEnabled } from '../utils/featureToggles';
@@ -12,31 +17,48 @@ type ProductsProps = {
 
 const Products: React.FC<ProductsProps> = ({ data }) => (
   <Layout>
-    <Container>
-      <Stack>
-        <Row>
-          <h1>Products</h1>
-        </Row>
-        <Row>
-          {!isFeatureEnabled(Feature.ProductsPage)
-            ? (
-              <h2>Na stránke pracujeme...</h2>
-            )
-            : (
-              <ul>
-                {data.allKontentItemProduct.edges.map(product => (
-                  <li key={product.node.id}>
+    <Section>
+      <SectionTitle>Produkty</SectionTitle>
+      <Container maxWidth="xl">
+        {!isFeatureEnabled(Feature.ProductsPage)
+          ? (
+            <Typography variant="h2">Na stránke stále pracujeme...</Typography>
+          )
+          : (
+            <Grid
+              container
+              spacing={4}
+              direction="row"
+              justify="flex-start"
+              alignItems="center"
+            >
+              {data.allKontentItemProduct.edges.map(product => (
+                <Grid
+                  key={product.node.id}
+                  item
+                  xl={2}
+                  lg={3}
+                  md={4}
+                  sm={6}
+                  xs={12}
+                >
+                  <Paper elevation={3} color="primary">
+                    <Img
+                      data-kontent-element-codename={`image`}
+                      fluid={product.node.elements?.image?.value?.[0]?.fluid as FluidObject}
+                      alt={product.node.elements?.image?.value?.[0]?.description ?? product.node.elements?.name?.value ?? 'Product image'}
+                    />
                     <Link to={PRODUCT_URL(product.node.elements?.url_slug?.value)}>
                       {product.node.elements?.name?.value}
                     </Link>
-                  </li>
-                ))}
-              </ul>
-            )
-          }
-        </Row>
-      </Stack>
-    </Container>
+                  </Paper>
+                </Grid>
+              ))}
+            </Grid>
+          )
+        }
+      </Container>
+    </Section>
   </Layout>
 );
 
@@ -54,6 +76,18 @@ export const query = graphql`
                         }
                         url_slug {
                             value
+                        }
+                        image {
+                            value {
+                                description
+                                fluid(maxWidth: 350) {
+                                    aspectRatio
+                                    base64
+                                    sizes
+                                    src
+                                    srcSet
+                                }
+                            }
                         }
                     }
                 }
