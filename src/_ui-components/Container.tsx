a27734b91cbd44e449276f9fbf2d93acd47c63c1
Zuzana Sojcikova <zuzanad@kentico.com>
2021-11-14 19:32:35 +0100

    Style Contact with Tailwind

--- src/_ui-components/Container.tsx
+++ src/_ui-components/Container.tsx
@@ -1,28 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: auto;
-  margin-left: auto;
-`;
-
-export const Stack = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-export const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-export const Main = styled.main`
-  width: 100%;
-`;
 
 export const Section: React.FC<{ className?: string, id: string }> = ({ children, id, className }) => (
   <section id={id} className={`py-md w-full ${className}`}>
--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,8 +1,5 @@
-import { Typography } from '@material-ui/core';
-import Container from '@material-ui/core/Container';
 import React from 'react';
-import styled from 'styled-components';
-import { Section, SectionTitle, Stack } from '../../_ui-components/Container';
+import { Section, SectionTitle } from '../../_ui-components/Container';
 import { EmailLink } from '../../_ui-components/EmailLink';
 import { Icon } from '../../_ui-components/Icon';
 import { NonBreakable } from '../../_ui-components/NonBreakable';
@@ -10,139 +7,85 @@ import { CompanyInfo, companyInfo, Contact, primaryContact, secondaryContact } f
 import { CONTACT_FRAGMENT } from '../../constants/urls';
 
 export const ContactSection: React.FC = () => (
-  <Section id={CONTACT_FRAGMENT} className="bg-primary">
-    <Container maxWidth="md">
-      <Stack>
-        <SectionTitle>Kontakt</SectionTitle>
-        <PersonsStyled>
-          <Person title="Konateľ:" person={primaryContact} />
-          <Person title="Spolumajiteľ:" person={secondaryContact} />
-        </PersonsStyled>
-        <SeparatorStyled />
-        <CompanyInfoStyled info={companyInfo} />
-      </Stack>
-    </Container>
+  <Section id={CONTACT_FRAGMENT} className="bg-primary leading-8 text-secondary-darker">
+    <div className="container-fluid space-y-md">
+      <SectionTitle>Kontakt</SectionTitle>
+      <div className="flex flex-wrap flex-row justify-evenly">
+        <Person title="Konateľ:" person={primaryContact} />
+        <Person title="Spolumajiteľ:" person={secondaryContact} />
+      </div>
+      <hr className="h-0.5 text-secondary-darker bg-secondary-darker border-0" />
+      <CompanyInfoComponent info={companyInfo} />
+    </div>
   </Section>
 );
 
-const PersonsStyled = styled.div`
-  // width: 100%;
-  // display: flex;
-  // flex-wrap: wrap;
-  // flex-direction: row;
-  // justify-content: space-evenly;
-  // color: ${p => p.theme.palette.secondary.main};
-  //
-  // & > * {
-  //   display: flex;
-  // }
-  //
-  // a {
-  //   color: ${p => p.theme.palette.secondary.main};
-  // }
-  //
-  // i {
-  //   margin-right: 0.8rem;
-  //   width: 1rem;
-  // }
-  //
-  // ul {
-  //   list-style: none;
-  //   padding-left: 0;
-  // }
-  //
-  // li {
-  //   margin: 1rem;
-  // }
-`;
-
 const Person: React.FC<{ readonly person: Contact, readonly title: string }> = ({ person, title }) => (
-  <Typography variant={'body2'} component="div">
+  <div className="">
     <ul>
       <li>{title}</li>
       <li>
-        <Icon type={'contact-person'} />&nbsp;
+        <Icon type="contact-person" />&nbsp;
         <b>{person.name}</b>
       </li>
       <li>
-        <Icon type={'email'}/>&nbsp;
-        <EmailLink email={person.email} />
+        <Icon type="email" />&nbsp;
+        <EmailLink email={person.email} className="text-secondary-dark" />
       </li>
       {person.phone && (
         <li>
-          <Icon type={'phone'} />&nbsp;
+          <Icon type="phone" />&nbsp;
           <NonBreakable>{person.phone}</NonBreakable>
         </li>
       )}
     </ul>
-  </Typography>
+  </div>
 );
 
 const CompanyInfoComponent: React.FC<{ readonly info: CompanyInfo }> = ({ info }) => (
-  <Typography variant={'body2'} component="div">
-    <DlStyled>
-      <DtStyled>Adresa</DtStyled>
-      <DdStyled>
-        {info.address.line1}
-        <br />
-        {info.address.line2}, {info.address.line3}
-      </DdStyled>
+  <div className="w-full flex justify-center">
+    <dl className="w-full flex flex-col">
+      <div className="flex">
+        <DtStyled>Adresa</DtStyled>
+        <DdStyled>
+          {info.address.line1}
+          <br />
+          {info.address.line2}, {info.address.line3}
+        </DdStyled>
+      </div>
 
-      <DtStyled>Email</DtStyled>
-      <DdStyled>
-        <EmailLink email={info.email} />
-      </DdStyled>
+      <div className="flex">
+        <DtStyled>Email</DtStyled>
+        <DdStyled>
+          <EmailLink email={info.email} className="text-secondary-dark" />
+        </DdStyled>
+      </div>
 
-      <DtStyled>IČO</DtStyled>
-      <DdStyled>{info.ico}</DdStyled>
+      <div className="flex">
+        <DtStyled>IČO</DtStyled>
+        <DdStyled>{info.ico}</DdStyled>
+      </div>
 
-      <DtStyled>DIČ</DtStyled>
-      <DdStyled>{info.dic}</DdStyled>
+      <div className="flex">
+        <DtStyled>DIČ</DtStyled>
+        <DdStyled>{info.dic}</DdStyled>
+      </div>
 
-      <DtStyled>IČ DPH</DtStyled>
-      <DdStyled>{info.icDph}</DdStyled>
-    </DlStyled>
-  </Typography>
+      <div className="flex">
+        <DtStyled>IČ DPH</DtStyled>
+        <DdStyled>{info.icDph}</DdStyled>
+      </div>
+    </dl>
+  </div>
 );
 
-const CompanyInfoStyled = styled(CompanyInfoComponent)`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-`;
-
-const DlStyled = styled.dl`
-  width: 100%;
-  display: flex;
-  flex-flow: row;
-  flex-wrap: wrap;
-  overflow: visible;
-  align-content: center;
-`;
-
-const DdStyled = styled.dd`
-  flex: 0 0 55%;
-  margin-left: auto;
-  margin-right: auto;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  text-align: left;
-`;
-
-const DtStyled = styled.dt`
-  flex: 0 0 35%;
-  margin-left: auto;
-  margin-right: auto;
-  text-align: right;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  //font-weight: ${p => p.theme.typography.fontWeightBold};
-`;
-
-const SeparatorStyled = styled.hr`
-  width: 100%;
-  //color: ${p => p.theme.palette.secondary.main};
-  border: 0;
-  height: 1px;
-  // background: ${p => p.theme.palette.secondary.main};
-`;
+const DdStyled: React.FC = ({ children }) => (
+  <dd className="mx-sm overflow-ellipsis text-left w-1/2">
+    {children}
+  </dd>
+);
+const DtStyled: React.FC = ({ children }) => (
+  <dt className="overflow-ellipsis text-right font-bold w-1/2">
+    {children}
+  </dt>
+);
