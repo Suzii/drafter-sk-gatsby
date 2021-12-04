79809cb7b935f6c1c2a12083563a2a2f202afee3
Zuzana Sojcikova <zuzanad@kentico.com>
2021-12-04 14:20:34 +0100

    Remove remains of MaterialUI

--- src/_ui-components/kontentSmartlink/KontentSmartLinksToggle.tsx
+++ src/_ui-components/kontentSmartlink/KontentSmartLinksToggle.tsx
@@ -1,9 +1,7 @@
 import KontentSmartLink from '@kentico/kontent-smart-link';
-import { IconButton } from '@material-ui/core';
-import { Close, Edit } from '@material-ui/icons';
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
 import { StringParam, useQueryParams } from 'use-query-params';
+import { Icon } from '../Icon';
 
 const queryParam = 'preview';
 
@@ -23,20 +21,13 @@ export const KontentSmartLinksToggle: React.FC = () => {
   const toggle = () => setValues({ [queryParam]: isOn ? undefined : null }, 'pushIn');
 
   return (
-    <ToggleStyled onClick={toggle}>
-      <IconButton color="primary">
-        {isOn ? <Close /> : <Edit />}
-      </IconButton>
-    </ToggleStyled>
+    <div
+      className="z-50 fixed right-md bottom-md"
+      onClick={toggle}
+    >
+      <button color="primary p-md">
+        {isOn ? <Icon type="x" /> : <Icon type="pencil" />}
+      </button>
+    </div>
   );
 };
-
-const ToggleStyled = styled.div`
-  z-index: ${p => p.theme.zIndex.snackbar};
-  position: fixed;
-  right: 2rem;
-  bottom: 2rem;
-  text-transform: uppercase;
-  background-color: ${p => p.theme.palette.secondary.main};
-  border-radius: 50%;
-`;
