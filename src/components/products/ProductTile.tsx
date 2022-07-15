c20c77c3edcad75544d095e7ada9ab2a7dbcf86b
Zuzana Sojcikova <zuzanad@kentico.com>
2022-07-15 11:39:55 +0200

    Improve product tile styling

--- src/components/products/ProductTile.tsx
+++ src/components/products/ProductTile.tsx
@@ -10,20 +10,20 @@ type Props<TGroupName extends AllKnownProductTaxonomies> = {
 };
 
 export const ProductTile = <TGroupName extends AllKnownProductTaxonomies>({ product }: Props<TGroupName>) => (
-  <div className="flex flex-col bg-white relative shadow-sm hover:shadow-lg transition duration-500">
-    <Link to={product.url}>
-      <ImageElement
-        className=""
-        image={product.img ?? {} as any}
-        alt={product.img?.description ?? product.name ?? 'Obrázok produktu'}
-      />
-    </Link>
-    <div className="py-sm px-sm bg-white">
-      <Link to={product.url}>
-        <div {...getKontentAttrs(product.id, 'name')} className="text-secondary text-left font-semibold text-md hover:text-secondary-dark hover:cursor-pointer">
-          {product.name}
-        </div>
-      </Link>
+  <Link
+    className="flex flex-col bg-white relative shadow-sm hover:shadow-lg transition duration-500"
+    to={product.url}
+    {...getKontentAttrs(product.id, 'name')}
+  >
+    <ImageElement
+      className=""
+      image={product.img ?? {} as any}
+      alt={product.img?.description ?? product.name ?? 'Obrázok produktu'}
+    />
+    <div className="mt-auto py-sm px-sm bg-white">
+      <div className="text-secondary text-left font-semibold text-md hover:text-secondary-dark hover:cursor-pointer">
+        {product.name}
+      </div>
     </div>
-  </div>
+  </Link>
 );
